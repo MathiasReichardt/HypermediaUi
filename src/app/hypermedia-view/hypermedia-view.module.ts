20dcbf6682eed9d3cd1c425584c101923fd5b96f
Mathias Reichardt <mathias.reichardt@posteo.de>
2017-10-22 13:21:44 +0200

    Put content in cards

--- src/app/hypermedia-view/hypermedia-view.module.ts
+++ src/app/hypermedia-view/hypermedia-view.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HypermediaControlComponent } from './hypermedia-control/hypermedia-control.component';
+import {MatCardModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   exports: [
     HypermediaControlComponent
